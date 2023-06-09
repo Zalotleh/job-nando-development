12e071feb8e5c5f653ff81a43d6ac539efe24bb4
Ziad Alotleh <ziadalotleh@gmail.com>
2023-06-09 22:02:02 +0200

    trying to a solve an error at building time at vercel, related to Navar imported module

--- components/landingPage/LandingPage.jsx
+++ components/landingPage/LandingPage.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import Navbar from '../Navbar'
 import styles from '../../styles/LandingPage.module.css'
 import Header from './Header'
 import ServicesSection from './ServicesSection'
 import BenefitsSection from './BenefitsSection'
 import PlansSection from './PlansSection'
+import Navbar from '../Navbar'
 import Footer from '../Footer'
 import QA from './Q&A'
 
 function LandingPage() {
   return (
     <body className={styles.mainPage}>
-      <Navbar />
+      <Navbar/>
       <main className={styles.main}>
         <Header/>
         <ServicesSection/>
