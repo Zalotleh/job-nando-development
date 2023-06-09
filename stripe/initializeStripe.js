40c9794dda22699581b3fd89ac64c2995318cddd
Ziad Alotleh <ziadalotleh@gmail.com>
2023-06-09 18:10:34 +0200

    fixing a bug

--- stripe/initializeStripe.js
+++ stripe/initializeStripe.js
@@ -5,7 +5,7 @@ let stripePromise= null;
 const initializeStripe = async () => {
   if (!stripePromise) {
     stripePromise = await loadStripe(
-      'pk_test_51N3IolL6LNzUqrJyucoGUgLcnBbavUL6lUzhugcemHbDeDes61HQJjUiN2aBzH3U0XgSt8Mgh3AI71vgR6EKE2tX005LFtNKql'
+      process.env.STRIPE_TEST_PUBLISHABLE_KEY
     );
   }
   return stripePromise;
